c2902c4de0423d6ecd5bb11428fca8f2411b24a8
Sai Pathuri <sai@pathuri.net>
2020-07-08 19:21:46 -0500

    Remove password when generating tokens

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -8,9 +8,11 @@ import User from "../model/User"
 import jsonwebtoken from "jsonwebtoken";
 
 export function generateAccessToken(user: User) {
+    delete user.password;
     return jsonwebtoken.sign(user, ACCESS_TOKEN_SECRET as string, { expiresIn: "60m" });
 }
 
 export function generateRefreshToken(user: User) {
+    delete user.password;
     return jsonwebtoken.sign(user, REFRESH_TOKEN_SECRET as string);
 }
